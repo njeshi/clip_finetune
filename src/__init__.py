e959511ed0b3d55998f89d45879df4f9f3377f64
Fabio <fabiobrahimi@hotmail.com>
2021-12-11 14:50:12 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .utils import TextTransformer
 from .dataset import FoodDataset, FoodDataModule, KPerClassSampler
 from .custom_clip import CLIP_Contrastive, CLIP_Linear
-from .custom_imagenet import ImageClassifier
+from .custom_imagenet_models import ImageClassifier
